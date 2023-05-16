72315ae1edf2734001f10e3243006b19340a25a4
Amilenkof <f.o.g@list.ru>
2023-05-16 19:45:32 +0400

    init commit

---
+++ src/main/java/com/coursework2/Coursework2Application.java
@@ -0,0 +1,13 @@
+package com.coursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
