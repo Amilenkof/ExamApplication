4280b30b907c70b6ebe0d6024684ffb4e26d7d4d
Amilenkof <f.o.g@list.ru>
2023-05-18 17:19:40 +0400

    basic done without unit-tests

--- src/main/java/com/coursework2/test.java
+++ src/main/java/com/coursework2/test.java
@@ -1,20 +1,15 @@
 package com.coursework2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class test {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(321);
-        list.add(12312);
-        list.add(12321312);
-        list.add(123563212);
-        list.add(124324312);
-        list.add(1312);
-
-        System.out.println("до удаления " + list);
-        list.remove(1);
-        System.out.println("после удаления " + list);
+//    public static void main(String[] args) {
+//      String s= """
+//              вфыв фа фвфаф """;
+//        HashSet <String> ss= Arrays.stream(s.split(" ")).collect(Collectors.toSet());
     }
-}
+
